126f37a64ce7485d5259d44f4aff8dd527d02505
Jayadev6191 <jaidev.akkiraju@gmail.com>
2016-05-28 11:50:30 -0700

    Added logs and structured html

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -8,8 +7,6 @@ var http = require('http');
 var dl  = require('delivery');
 var fs  = require('fs');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 var server=http.createServer(app);
@@ -19,16 +16,13 @@ var io = require('socket.io').listen(server);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,10 +34,12 @@ app.use(function(req, res, next) {
  
 io.sockets.on('connection', function(socket){
   console.log("socket connected");
+
   var delivery = dl.listen(socket);
   delivery.on('receive.success',function(file){
     var params = file.params;
-    console.log(file);
+    console.log("Received File");
+    
     fs.writeFile(file.name,file.buffer, function(err){
       if(err){
         console.log('File could not be saved.');
